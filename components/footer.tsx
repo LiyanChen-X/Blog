4435754c3eb50cfd68b5ed45da348c45eb6e1768
liych <liyanchen128@outlook.com>
2022-08-21 18:27:19 +0800

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 const Footer = () => {
     return (
         <Box as={"footer"} textAlign="center" opacity={0.4} fontSize="sm">
-            &copy; {new Date().getFullYear()} Liych&apos;s Blog.
+            &copy; {new Date().getFullYear()} Liyan&apos;s Blog.
         </Box>
     )
 }
